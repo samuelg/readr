773d5c6c70e453500f7145c1f0826476251c5492
Samuel Galarneau <samuelg@samuelg-laptop.(none)>
2009-04-24 23:03:13 -0500

    Added code to server static media in dev mode. Added base style sheet.

--- settings.py
+++ settings.py
@@ -43,17 +43,17 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'srx(+z812-!%-ifhe@a=8ky52ladnk5f5!00_^33iis&sin8)z'
@@ -62,11 +62,11 @@ SECRET_KEY = 'srx(+z812-!%-ifhe@a=8ky52ladnk5f5!00_^33iis&sin8)z'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
+    'django.core.context_processors.media',
 )
 
 MIDDLEWARE_CLASSES = (
--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-admin.autodiscover()
+import settings
 import views
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
@@ -10,3 +12,12 @@ urlpatterns = patterns('',
     url(r'^register/$', views.register_user, name='register'),
     url(r'^logout/$', views.logout_user, name='logout'),
 )
+
+# setup to server static files in development mode
+if settings.DEBUG:
+    if settings.MEDIA_URL.startswith("/"):
+        media_url = settings.MEDIA_URL[1:]
+        urlpatterns += patterns('',
+            (r'^%s(?P<path>.*)$' % media_url, 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        )
+
