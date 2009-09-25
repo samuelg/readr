7bae4a05395d736dbbccae848549987b5bcdd8a7
Samuel Galarneau <samuelg@samuelg-laptop.(none)>
2009-09-25 10:31:41 -0500

    Added more tests to publications app

--- publications/tests.py
+++ publications/tests.py
@@ -1,17 +1,37 @@
 from django.test import TestCase
-from publications.models import Publication
+from publications.models import Publication, Reading
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
-class PublicationTestCase(TestCase):
+class ReadingModelTestCase(TestCase):
     fixtures = ['publications']
     
     def setUp(self):
-        pass
+        user = User(pk=1, username='samuel')
+        user.save()
 
-    def tearDown(self):
-        pass
-
-    def testPublication(self):
+    def testReadingUniqueness(self):
+        """ Ensures readings are unique """
         pub = Publication.objects.get(pk=1)
-        self.assertEquals(pub.title, 'something')
+        user = User.objects.get(pk=1)
+        reading = Reading(publication=pub, rating=1, user=user)
+        self.assertRaises(IntegrityError, reading.save)
 
+    def testReadingUnicode(self):
+        """ Ensures __unicode__ returns expected output """
+        reading = Reading.objects.get(pk=1)
+        self.assertEquals(str(reading), 'samuel has read something')
 
+class PublicationModelTestCase(TestCase):
+    fixtures = ['publications']
+
+    def testPublicationUnicode(self):
+        """ Ensures __unicode__ returns expected output """
+        pub = Publication.objects.get(pk=1)
+        self.assertEquals(str(pub), 'something')
+
+class PublicationsViewsTestCase(TestCase):
+    fixtures = ['publications']
+
+    def testView(self):
+        pass
